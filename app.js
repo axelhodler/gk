e03bb3f30e3fcf9b1a57e56eb5a19d93485af24d
xorrr <derxor@gmail.com>
2015-08-19 21:49:40 +0200

    get restaurants from api

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 (function() {
-  var app = angular.module('gourmetklub', ['uiGmapgoogle-maps']);
+  var app = angular.module('gourmetklub', ['uiGmapgoogle-maps', 'ngResource']);
 
+  app.factory('Restaurant', function($resource) {
+    return $resource('http://localhost:5111/restaurants');
+  });
 
-  app.controller('GourmetklubController', function($scope, uiGmapGoogleMapApi) {
-    this.restaurants = restaurants;
+  app.controller('GourmetklubController', function($scope, uiGmapGoogleMapApi, Restaurant) {
+    Restaurant.get(function(data) {
+      $scope.restaurants = data.restaurants;
+    });
 
     uiGmapGoogleMapApi.then(function(maps) {
       $scope.key = "lol";
