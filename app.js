bc0b19413927e442ac8ce0b8daa3650d981f80d6
xorrr <derxor@gmail.com>
2015-10-18 23:00:53 +0200

    display a marker for each restaurant

--- app.js
+++ app.js
@@ -11,8 +11,6 @@
     });
 
     uiGmapGoogleMapApi.then(function(maps) {
-      $scope.key = "lol";
-      $scope.coords = { latitude: 48.775, longitude: 9.183 };
       $scope.map = { center: { latitude: 48.775 , longitude: 9.183 }, zoom: 13 };
     });
   });
