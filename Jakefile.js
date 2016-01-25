924049f14ec978f03eaa2d9e07807e2f5d211c80
Axel Hodler <derxor@gmail.com>
2016-01-25 22:28:17 +0100

    can minify source

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,23 @@
 (function () {
   'use strict';
 
-  desc("says hello")
+  var fs = require("fs");
+  var uglify = require("uglify-js");
+
+  desc("minifies source")
   task('default', function() {
-    console.log('hello');
+    var result = uglify.minify([
+      "bower_components/angular/angular.js",
+      "bower_components/angular-resource/angular-resource.js",
+      "bower_components/lodash/lodash.js",
+      "bower_components/angular-google-maps/dist/angular-google-maps.js",
+      "bower_components/angular-simple-logger/dist/angular-simple-logger.js",
+      "src/app.js"]);
+
+    fs.writeFile('app.min.js', result.code, function(err) {
+      if (err) throw err;
+      console.log("minified js source created");
+    });
   });
 
 }());
