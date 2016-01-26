2256f5f582c2853c995ea78e81f3fd9cfd213699
Axel Hodler <derxor@gmail.com>
2016-01-26 20:24:02 +0100

    add html-replace task

--- Jakefile.js
+++ Jakefile.js
@@ -3,9 +3,10 @@
 
   var fs = require("fs");
   var uglify = require("uglify-js");
+  var replace = require('replace');
 
   var VENDOR_DIR = "vendor";
-  desc("minifies source")
+  desc("minifies source");
   task('default', function() {
     var result = uglify.minify([
       VENDOR_DIR + "/angular/angular.js",
@@ -19,6 +20,15 @@
       if (err) throw err;
       console.log("minified js source created");
     });
-  });
+  })
 
+  desc('replace libs in index.html with uglified');
+  task('html-replace', function() {
+    replace({
+      regex: '<!-- begin_autoreplace_with_uglyfied -->(.|\n)*<!-- end -->',
+      replacement: '<script src="app.min.js"></script>',
+      paths: ['index.html'],
+      silent: true
+    });
+  });
 }());
