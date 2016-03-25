a66937c93dc24e95856c286b99e2b90b3d2848f9
Axel Hodler <derxor@gmail.com>
2016-03-25 19:54:34 +0100

    wrap tests in describe blocks

--- src/login/login_test.js
+++ src/login/login_test.js
@@ -3,15 +3,17 @@
 (function() {
   'use strict';
 
-  var compile,
-    scope;
-
-  beforeEach(inject(function(_$rootScope_, _$compile_) {
-    scope = _$rootScope_;
-    compile = _$compile_;
-  }));
-
   describe('Login', function() {
+    var compile,
+      scope;
+
+    beforeEach(module('gourmetklub'));
+
+    beforeEach(inject(function(_$rootScope_, _$compile_) {
+      scope = _$rootScope_;
+      compile = _$compile_;
+    }));
+
     it('provides a login form', function() {
       var element = compile("<div gk-login></div>")(scope);
 
