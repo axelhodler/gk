e001544936f356f6b27689d088cb20f6c707e082
Axel Hodler <derxor@gmail.com>
2016-03-26 10:46:22 +0100

    login is a post request

--- src/login/services/login_request.js
+++ src/login/services/login_request.js
@@ -2,7 +2,11 @@
   'use strict';
 
   angular.module('login')
-    .service('loginService', function() {
-
+    .service('loginService', function($http) {
+      return {
+        login: function(username, password) {
+          $http.post('http://gkapi.hodler.co:5111/login');
+        }
+      }
     });
 })();
--- src/login/services/login_request_test.js
+++ src/login/services/login_request_test.js
@@ -2,16 +2,21 @@
   'use strict';
 
   describe('LoginService', function() {
-    var loginService;
+    var loginService,
+      httpBackend;
 
     beforeEach(module('login'));
 
-    beforeEach(inject(function(_loginService_) {
+    beforeEach(inject(function(_loginService_, _$httpBackend_) {
       loginService = _loginService_;
+      httpBackend = _$httpBackend_;
     }));
 
     it('exists', function() {
+      loginService.login('username', 'password');
 
+      httpBackend.expectPOST('http://gkapi.hodler.co:5111/login').respond({});
+      httpBackend.flush();
     });
   });
 })();
