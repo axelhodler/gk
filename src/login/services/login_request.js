fc8080df83139e3bbc4d11c9a3cf0d2bdba41986
Axel Hodler <derxor@gmail.com>
2016-03-26 10:34:59 +0100

    add login request file structure

---
+++ src/login/services/login_request.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  angular.module('login')
+    .service('loginService', function() {
+
+    });
+})();
---
+++ src/login/services/login_request_test.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  describe('LoginService', function() {
+    var loginService;
+
+    beforeEach(module('login'));
+
+    beforeEach(inject(function(_loginService_) {
+      loginService = _loginService_;
+    }));
+
+    it('exists', function() {
+
+    });
+  });
+})();
