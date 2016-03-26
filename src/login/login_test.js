3ef3e0f271b47f4968f7e65f2ce5fd02ddb84800
Axel Hodler <derxor@gmail.com>
2016-03-26 01:19:19 +0100

    rm duplicated tests

--- src/login/login_test.js
+++ src/login/login_test.js
@@ -21,27 +21,6 @@
       return compile('<div gk-login></div>')(scope);
     };
 
-    it('provides a login form', function() {
-      var element = compileLoginForm();
-
-      scope.$digest();
-
-      var html = element.html();
-      expect(html).toContain('<form');
-      expect(html).toContain('</form>');
-    });
-
-    it('has two input fields', function() {
-      var element = compileLoginForm();
-
-      scope.$digest();
-
-      var html = element.html();
-      expect(html).toContain('<input');
-      html.replace('<input', '');
-      expect(html).toContain('<input');
-    });
-
     it('has one password field', function() {
       var element = compileLoginForm();
 
