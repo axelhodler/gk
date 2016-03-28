b1ce2973bb3ea841b56e532a9c64255f19818069
Axel Hodler <derxor@gmail.com>
2016-03-28 12:30:50 +0200

    unify quotes in jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -2,69 +2,69 @@
 (function () {
   'use strict';
 
-  var fs = require("fs");
-  var uglify = require("uglify-js");
+  var fs = require('fs');
+  var uglify = require('uglify-js');
   var replace = require('replace');
-  var karma = require("simplebuild-karma");
-  var jshint = require("simplebuild-jshint");
-  var shell = require("shelljs");
+  var karma = require('simplebuild-karma');
+  var jshint = require('simplebuild-jshint');
+  var shell = require('shelljs');
 
-  var KARMA_CONFIG = "karma.conf.js";
-  var VENDOR_DIR = "vendor";
-  var DIST_DIR = "dist";
-  var MINIFIED_APP = "app.min.js";
+  var KARMA_CONFIG = 'karma.conf.js';
+  var VENDOR_DIR = 'vendor';
+  var DIST_DIR = 'dist';
+  var MINIFIED_APP = 'app.min.js';
   var INDEX_HTML = 'index.html';
   var GENERATED_INDEX_HTML = 'generated_index.html';
 
   var SMOKE_TASK = 'smoke';
 
-  desc("minifies source");
+  desc('minifies source');
   task('minify', function() {
     var result = uglify.minify([
-      VENDOR_DIR + "/angular/angular.js",
-      VENDOR_DIR + "/angular-resource/angular-resource.js",
-      VENDOR_DIR + "/lodash/lodash.js",
-      VENDOR_DIR + "/angular-google-maps/dist/angular-google-maps.js",
-      VENDOR_DIR + "/angular-simple-logger/dist/angular-simple-logger.js",
+      VENDOR_DIR + '/angular/angular.js',
+      VENDOR_DIR + '/angular-resource/angular-resource.js',
+      VENDOR_DIR + '/lodash/lodash.js',
+      VENDOR_DIR + '/angular-google-maps/dist/angular-google-maps.js',
+      VENDOR_DIR + '/angular-simple-logger/dist/angular-simple-logger.js',
       'src/app.js',
       'src/common/common.js',
       'src/map/restaurant_map.js']);
 
     fs.writeFile(MINIFIED_APP, result.code, function(err) {
       if (err) throw err;
-      console.log("minified js source created");
+      console.log('minified js source created');
     });
   });
 
-  desc("Creates directory containing distributable files");
-  task("package", ['minify','html-replace'], function() {
-    console.log("Building distribution directory: .");
+  desc('Creates directory containing distributable files');
+  task('package', ['minify','html-replace'], function() {
+    console.log('Building distribution directory: .');
 
-    shell.rm("-rf", DIST_DIR + "/*");
+    shell.rm('-rf', DIST_DIR + '/*');
     var templateDir = 'dist/src/map/templates';
     shell.mkdir('-p', templateDir);
     shell.cp(MINIFIED_APP, DIST_DIR);
-    shell.mv(GENERATED_INDEX_HTML, DIST_DIR + "/" + INDEX_HTML);
-    shell.cp("src/map/templates/restaurant.html", templateDir);
-    shell.cp("src/map/templates/map_marker_window.html", templateDir);
-    shell.cp("style.css", DIST_DIR);
+    shell.mv(GENERATED_INDEX_HTML, DIST_DIR + '/' + INDEX_HTML);
+    shell.cp('src/map/templates/restaurant.html', templateDir);
+    shell.cp('src/map/templates/map_marker_window.html', templateDir);
+    shell.cp('style.css', DIST_DIR);
   });
 
-  desc("Lint JavaScript code");
-  task("lint", function() {
-    process.stdout.write("Linting JavaScript: ");
+  desc('Lint JavaScript code');
+  task('lint', function() {
+    process.stdout.write('Linting JavaScript: ');
 
     jshint.checkFiles({
-      files: [ "Jakefile.js", "src/**/*.js" ],
+      files: [ 'Jakefile.js', 'src/**/*.js' ],
       options: lintOptions(),
       globals: lintGlobals()
     }, complete, fail);
   }, { async: true });
 
 
-  desc("Start the Karma server (run this first)");
-  task("karma", function() {
-    console.log("Starting Karma server:");
+  desc('Start the Karma server (run this first)');
+  task('karma', function() {
+    console.log('Starting Karma server:');
     karma.start({
       configFile: KARMA_CONFIG
     }, complete, fail);
@@ -101,7 +101,7 @@
       forin: true,
       freeze: true,
       futurehostile: true,
-      latedef: "nofunc",
+      latedef: 'nofunc',
       noarg: true,
       nocomma: true,
       nonbsp: true,
