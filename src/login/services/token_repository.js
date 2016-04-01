3b2ee6bdef7a99baace8f04e88f181de94eec99d
Axel Hodler <derxor@gmail.com>
2016-04-01 20:21:09 +0200

    can store tokens

--- src/login/services/token_repository.js
+++ src/login/services/token_repository.js
@@ -2,7 +2,11 @@
   'use strict';
 
   angular.module('gk.login')
-    .service('TokenRepository', function() {
-      
+    .service('TokenRepository', function(LocalForage) {
+      return {
+        store: function(token) {
+          LocalForage.setItem('jwt', token);
+        }
+      }
     });
 })();
--- src/login/services/token_repository_test.js
+++ src/login/services/token_repository_test.js
@@ -2,16 +2,29 @@
   'use strict';
 
   describe('TokenRepository', function() {
-    var TokenRepo;
+    var TokenRepo,
+      storedToken;
 
     beforeEach(module('gk.login'));
+
+    beforeEach(module(function($provide) {
+      var LocalForageMock = {
+        setItem: function(key, value) {
+          storedToken = value;
+          this.key = value;
+        }
+      };
+      $provide.value("LocalForage", LocalForageMock);
+    }));
 
     beforeEach(inject(function(_TokenRepository_) {
       TokenRepo = _TokenRepository_;
     }));
 
     it('can store tokens', function() {
+      TokenRepo.store('token');
 
+      expect(storedToken).toBe('token');
     });
   })
 })();
