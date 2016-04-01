31f0dc14ae26744c2d78bfa08edfac9f07515434
Axel Hodler <derxor@gmail.com>
2016-04-01 21:46:39 +0200

    store webtoken after successful login

--- src/login/services/login_request_test.js
+++ src/login/services/login_request_test.js
@@ -5,11 +5,20 @@
     var loginService,
       httpBackend,
       restUrl,
-      Credentials;
+      Credentials,
+      TokenRepoSpy;
 
     beforeEach(function() {
       module('gk.login');
     });
+
+    beforeEach(module(function($provide) {
+      TokenRepoSpy = {
+        store: function() {}
+      };
+      spyOn(TokenRepoSpy, 'store');
+      $provide.value('TokenRepository', TokenRepoSpy);
+    }));
 
     beforeEach(inject(function(_loginService_, _$httpBackend_, _Credentials_, _REST_URL_) {
       loginService = _loginService_;
@@ -18,13 +27,33 @@
       Credentials = _Credentials_;
     }));
 
-    it('provides credentials to the server', function() {
-      var credentials = new Credentials('username', 'password');
+    var credentials;
 
+    beforeEach(function() {
+      credentials = new Credentials('username', 'password');
+    });
+
+    it('provides credentials to the server', function() {
       loginService.login(credentials);
 
       httpBackend.expectPOST(restUrl + '/login', credentials).respond({});
       httpBackend.flush();
+    });
+
+    it('stores webtoken after login', function() {
+      loginService.login(credentials);
+      httpBackend.expectPOST(restUrl + '/login', credentials).respond(200, 'token');
+      httpBackend.flush();
+
+      expect(TokenRepoSpy.store).toHaveBeenCalledWith('token');
+    });
+
+    it('stores webtoken after successful login', function() {
+      loginService.login(credentials);
+      httpBackend.expectPOST(restUrl + '/login', credentials).respond(200, 'webtoken');
+      httpBackend.flush();
+
+      expect(TokenRepoSpy.store).toHaveBeenCalledWith('webtoken');
     });
   });
 })();
