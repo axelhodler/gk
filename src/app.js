c60aa7e600dc5aaff2ff70f459718c803365cd21
Axel Hodler <derxor@gmail.com>
2016-04-02 11:06:26 +0200

    define login routing

--- src/app.js
+++ src/app.js
@@ -4,5 +4,11 @@
   angular.module('gourmetklub', [
     'ngRoute',
     'gk.restaurantMap'
-  ]);
+  ])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/login', {
+        templateUrl: 'login/templates/login-page.html',
+        controller: 'LoginPageController'
+      });
+    }]);
 })();
