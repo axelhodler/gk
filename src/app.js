f54aa56c08f7d5c01b1c9ccb19bc7da979e32329
Axel Hodler <derxor@gmail.com>
2016-04-02 18:28:58 +0200

    move map overview to its own template

--- src/app.js
+++ src/app.js
@@ -7,9 +7,14 @@
     'gk.login'
   ])
     .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/login', {
-        templateUrl: 'src/login/templates/login_page.html',
-        controller: 'LoginPageController'
-      });
+      $routeProvider
+        .when('/', {
+          templateUrl: 'src/map/templates/map_overview',
+          controller: 'GourmetklubController'
+        })
+        .when('/login', {
+          templateUrl: 'src/login/templates/login_page.html',
+          controller: 'LoginPageController'
+        });
     }]);
 })();
